67b0a1cd819db95b3c816a1c0ed742af74a20dee
Brian Chen <32105182+BriWho@users.noreply.github.com>
2021-01-21 01:39:17 +0800

    Add files via upload

--- Problem Set Volumes 100 - 199/142 - Mouse Clicks/main.c
+++ Problem Set Volumes 100 - 199/142 - Mouse Clicks/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<limits.h>
 #define MAXICONS 100
 #define MAXREGIONS 100
@@ -14,27 +15,33 @@ int iconDist(int x , int y , int icon[2]){
 int main(){
 	int regions[MAXREGIONS][4];
 	int icons[MAXICONS][2];
+	int iscover[MAXICONS];
+	int ans[MAXICONS];
 
 	char type[2];
-	int r = 0, i = 0;
+	int r = 0, i = 0 , a = 0;
 	int x, y , j, k;
+
+	memset(iscover , 0 , sizeof(iscover));
 
 	while(scanf("%s" , type)!=EOF){
 		if(type[0] == '#') break;
 		else if(type[0] == 'I'){
 			scanf("%d%d" , &icons[i][0] , &icons[i][1]);
+
 			for(j = 0 ; j < r ; j++){
 				if(inRegion(icons[i][0] , icons[i][1] , regions[j])){
-					icons[i][0] = icons[i][1] = -1;
+					iscover[i] = 1;
 					break;
 				}
 			}
 			i++;
 		}else if(type[0] == 'R'){
 			scanf("%d%d%d%d" , &regions[r][0] , &regions[r][1] , &regions[r][2] , &regions[r][3]);
+
 			for(j = 0 ; j < i ; j++){
 				if(inRegion(icons[j][0] , icons[j][1] , regions[r])){
-					icons[j][0] = icons[j][1] = -1;
+					iscover[j] = 1;
 				}
 			}
 			r++;
@@ -51,17 +58,14 @@ int main(){
 				}
 			}
 			for(j = 0 ; j < i && !isInRegion; j++){
-				if(icons[j][0] < 0 || icons[j][1] < 0)
-					continue;
+				if(iscover[j]) continue;
 				int dist2 = iconDist(x , y , icons[j]);
-				if(minDist > dist2) minDist = dist2; 
+				if(minDist > dist2) minDist = dist2, a = 0;
+				if(minDist >= dist2) ans[a++] = j;
 			}
-			for(j = 0 ; j < i && !isInRegion ; j++){
-				if(icons[j][0] < 0 || icons[j][1] < 0)
-					continue;
-				int dist2 = iconDist(x , y , icons[j]);
-				if(minDist == dist2) printf("%3d" , j +1); 
-			}
+
+			for(j = 0 ; j < a && !isInRegion; j++)
+				printf("%3d" , ans[j] +1);
 			puts("");
 		}
 
