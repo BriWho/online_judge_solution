07c51ab7feed229f2b503a3a3c50658085cb2cbf
Brian Chen <32105182+BriWho@users.noreply.github.com>
2021-01-28 15:42:37 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+int bfs(int x , int y , int k , int m , int n){
+	if(k < 1) return 0;
+	return bfs(x - k, y - k, k/2, m, n)+
+		bfs(x + k, y - k, k/2, m, n) +
+		bfs(x + k, y + k, k/2, m, n) +
+		bfs(x - k, y + k, k/2, m, n) +
+		(x - k <= m && x + k >= m && y - k <= n && y + k >= n);
+}
+
+int main(){
+	int i;
+	int m, n , k;
+
+	while(scanf("%d%d%d" , &k , &m , &n) == 3){
+		if(k == 0 && m == 0 && n == 0) break;
+		printf("%3d\n" , bfs(1024 , 1024 , k , m, n));
+	}
+
+	return 0;
+}
