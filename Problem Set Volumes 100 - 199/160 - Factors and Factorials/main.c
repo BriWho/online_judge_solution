30a0e80a8d48f5556c2cf211a63f766a85bb2bf1
Brian Chen <32105182+BriWho@users.noreply.github.com>
2021-02-02 21:26:16 -0800

    Add files via upload

--- Problem Set Volumes 100 - 199/160 - Factors and Factorials/main.c
+++ Problem Set Volumes 100 - 199/160 - Factors and Factorials/main.c
@@ -5,24 +5,24 @@
 int main(){
 	int N;
 	int i , j;
-	int prime[128];
+	int factor[128];
 
 	while(scanf("%d" , &N) !=EOF){
 		if(N == 0) break;
 		for(i = 0 ; i <= N ; i++)
-			prime[i] = i;
+			factor[i] = i;
 		printf("%3d! =" , N);
 		int primeNum = 0;
 		for(i = 2 ; i <= N ; i++){
-			if(prime[i] == 1) continue;
+			if(factor[i] == 1) continue;
 			if(primeNum == 15){
 				printf("\n      ");
 				primeNum = 0;
 			}
 			int cnt = 0;
 			for(j = i ; j <= N ; j++){
-				while(prime[j] % i == 0)
-					cnt++ , prime[j] /= i;
+				while(factor[j] % i == 0)
+					cnt++ , factor[j] /= i;
 			}
 			printf("%3d" , cnt);
 			primeNum++;
