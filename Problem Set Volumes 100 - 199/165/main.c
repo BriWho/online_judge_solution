2eec6c1b23cd6d920e1ce225f70d98ad35a36d7d
Brian Chen <32105182+BriWho@users.noreply.github.com>
2025-03-15 11:35:10 +0800

    save main.c

--- Problem Set Volumes 100 - 199/165/main.c
+++ Problem Set Volumes 100 - 199/165/main.c
@@ -1,70 +1,61 @@
 #include <stdio.h>
+#include <string.h>
 
 char dp[16][256];
+int max_stamps[16];
+int H, K, N;
 
-int lar_cont_seq(int H, int stamps[], int idx) {
+int cont_seq(int stamps[], int idx) {
     int i, j , k;
     memset(dp, 0 , sizeof(dp));
     dp[0][0]=1;
     for(i = 1;i <= H; i++){
-        memcpy(dp[i], dp[i-1], sizeof(char) * 256);
+        memcpy(dp[i], dp[i-1], sizeof(char) * stamps[idx] * i);
         for(j = 0; j <= idx ;j++)
             for(k = stamps[j]; k <= stamps[idx]*i;k++)
                 dp[i][k] = dp[i-1][k-stamps[j]];
     }
 
-    int v = 0；
-    while(dp[H][v] != 0)
-        v++;
-    return v;
+    int val = 0;
+    while(dp[H][val] != 0)
+        val++;
+    return val;
 
 }
 
-int dfs(int H, int K, int stamps[], int idx , int max_stamps[], int h) {
+void dfs(int stamps[], int idx , int max) {
 
     if( idx == K ) {
-        if( h )
-        for(i = 0; i<K;i++)
-            max_stamps[i]=stamps[i];
-        return 0;
+        if( max > N){
+            memcpy(max_stamps, stamps, sizeof(int) * K);
+            N = max;
+        }
+        return;
     }
-    
-    int i , max=0;
-    for(i=stamps[idx-1]+1;i<= h+1;i++){
-        stamps[idx]=i;
-        int r = dfs(H, K, stamps, idx+1, max_stamps,lar_cont_seq(H, stamps, idx));
-        if(r > max)
-            max = r;
-    }
-    
-    return max;
-    
-}
 
-int n(int H , int K , int max_stamps[]){
-    
-    int stamps[16];
-    stamps[0]=1;
-    
-    return dfs(H , K , stamps , 1 , max_stamp, H );
-    
+    int i;
+    for(i=stamps[idx-1]+1;i<= max+1;i++){
+        stamps[idx]=i;
+        dfs(stamps, idx+1, cont_seq(stamps, idx));
+    }
+
 }
 
 int main() {
-    int H, K;
-    int max_stamps[16];
+    int stamps[16];
     int i;
     while(scanf("%d %d", &H, &K)==2){
         if(H==0 && K==0){
             break;
         }
-        
-        int r = n(H , K , max_stamps);
-        
+        stamps[0] = 1;
+        N = 0;
+        dfs(stamps, 1, H);
+
         for(i=0; i<H ;i++){
-            printf("%3d" , max_stamps[i]);
+            printf("%3d", max_stamps[i]);
         }
-        printf("->%3d\n" , r );
+        printf("->%3d\n" , N );
     }
 
     return 0;
