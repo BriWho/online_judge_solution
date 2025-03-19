c6e2fa386a4d01d2096d2a2768b520882343d50d
Brian Chen <32105182+BriWho@users.noreply.github.com>
2025-03-19 23:18:15 +0800

    Update main.c

--- Problem Set Volumes 100 - 199/169 - Xenosemantics/main.c
+++ Problem Set Volumes 100 - 199/169 - Xenosemantics/main.c
@@ -3,25 +3,50 @@
 #include <string.h>
 
 struct Word {
-    char sorted[256];
+    int a[26];
     int start[512];
-    int end[512];
+    int len;
+    int count;
 };
 
-struct Word* words[1024];
-int words_count = 0;
+struct Word words[1026];
+int n_words = 0;
+char overlap[1026][1026];
 
-struct Word create_word(char* str, int s , int e) {
+int word_cmp(char* w1 , char* w2) {
+    int i;
+    for(i = 0 ; i < 26 ; i++)
+        if(w1 -> a[i] != w2 -> a[i])
+            return 1;
+    return 0;
+}
+
+void insert_new_word(struct Word * w) {
+    int i , j;
+    for(i = 0 ; i < n_words ; i++ )
+        if(words[i].len == w -> len && word_cmp(w , &words[i]) == 0)
+            break;
+    if( i < n_words )
+        for(j = 0 ; j < w -> count ; j++)
+            words[i].start[words[i].count++] = w -> start[j]; 
+    else
+        memcpy(words[n_words++] ,  w , sizeof(struct Word));
+}
+
+int filter_repeat_words() {
+    int i , n = 0;
+    for(i = 0 ; i < n_words ; i++)
+        if(words[i].count >= 2)
+            memcpy(&words[n++] , &word[i] , sizeof(struct Word));
+}
+
+int is_overlap(char* w1 , char * w2 ){
     
 }
 
-void search_word(char* msg) {
-    int i , j;
-    int len = strlen(msg);
-    
-    for(i = 0 ; i < len ; i++){
-        
-    }
+void filter_overlap_words() {
+    int i , j , n = 0; 
+    memset( overlap , 0 , sizeof(overlap));
 }
 
 
@@ -45,6 +70,56 @@ int main() {
         if(buf[b_len - 1] == '-') 
             continue;
         
+        int j , k;
+        struct Word word;
+    
+        for(i = 0 ; i < 26 ; i++){
+            memset(word , 0 , sizeof(struct Word));
+            
+            for(j = 0 ; j < m_len ; j++){
+                if(msg[j] == 'a' + i) break;
+                word.a[msg[j] - 'a']++;
+            }
+            
+            if(j < m_len && j >= 2 && j <= 250){
+                word.len = j;
+                word.start[0] = 0;
+                word.count = 1;
+                
+                insert_new_word(word);
+            }
+        }
+    
+        for(i = 0 ; i < m_len ; i++){
+
+            memset(word , 0 , sizeof(struct Word));
+            
+            for(j = i + 1 ; j < m_len ; j++){
+                if(msg[j] == msg[i]) break;
+                word.a[msg[j] - 'a']++;
+            }
+            
+            int len = j - i - 1;
+            if(len >= 2 && len <= 250) {
+                word.len = len;
+                word.start[0] = j;
+                word.count = 1;
+                
+                insert_new_word(word);
+            }
+            
+        }
+        
+        filter_repeat_words();
+        filter_overlap_words();
+
+
+        for(i = 0 ; i < n_words ; i++){
+            int start = words[i].start;
+            for(j = 0 ; j < words[i].len ; j++)
+                printf("%c" , msg[start + j]);
+            printf("\n");
+        }
         
         printf("%s\n" , msg);
         
@@ -53,4 +128,5 @@ int main() {
     }
 
     return 0;
+}
 }
