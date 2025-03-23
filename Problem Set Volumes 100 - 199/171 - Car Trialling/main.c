b7c9b029dfae583eda0e57918694ebb992090eb7
Brian Chen <32105182+BriWho@users.noreply.github.com>
2025-03-23 13:40:16 +0800

    Update main.c

--- Problem Set Volumes 100 - 199/171 - Car Trialling/main.c
+++ Problem Set Volumes 100 - 199/171 - Car Trialling/main.c
@@ -5,16 +5,16 @@
 
 #define MAX_LENGTH 128
 
-int match_string(char* input , int L , int R , char* str) {
-    int i , j , match_idx = 0;
-    int len = strlen(str);
-    for(i = L ; i < R ; i++){
-        if(input[i] == str[match_idx]){
-            match_idx++;
-            if(len == match_idx )
-                return i - len + 1;
+int find_text_from_right(char* input , int L , int R , char* str) {
+    int i , j , len = strlen(str);
+    int match_idx = len;
+    for(i = R - 1 ; i >= L ; i--){
+        if(input[i] == str[match_idx - 1]){
+            match_idx--;
+            if(0 == match_idx )
+                return i;
         } else 
-            match_idx = 0;
+            match_idx = len;
     }
     return -1;
 }
@@ -40,6 +40,16 @@ int trim_and_call(int (*parse)(char* , int , int , char* , int) ,
     return parse(input , trim_left(input , L , R ) , trim_right(input , L , R) , output , OL);
 }
 
+int parse_bold_text(char* input , int L , int R , char* output , int OL , char* text) {
+    int len = strlen(text);
+    if(len + L == R && L == find_text_from_right(input , L , R , text)){
+        memcpy(output + OL , text , sizeof(char) * len);
+        return OL + len; 
+    }
+    return -1;
+}
+
+
 int is_digit(char d){
     return '0' <= d && d <= '9';
 }
@@ -54,23 +64,20 @@ int parse_nnn(char* input , int L , int R , char* output , int OL){
     return OL + R - L;
 }
 int parse_cas(char* input , int L , int R ,char* output , int OL) {
-    if(R - L == 20 && match_string(input , L , R , "CHANGE AVERAGE SPEED") == L){
-        memcpy( output + OL , "CHANGE AVERAGE SPEED" , sizeof(char) * 20);
-        return OL + 20;
-    }
-    if(R - L == 3 && match_string(input , L , R , "CAS") == L){
-        memcpy( output + OL , "CAS" , sizeof(char) * 3);
-        return OL + 3;
-    }
-    return -1;
+    int change_average_speed_tail = parse_bold_text(input ,L , R ,output , OL , "CHANGE AVERAGE SPEED");
+
+    if(change_average_speed_tail != -1)
+        return change_average_speed_tail;
+
+    return parse_bold_text(input , L , R , output , OL , "CAS");
 }
 int parse_change(char* input , int L , int R , char* output , int OL){
-    int to_head = match_string(input , L , R , "TO");
+    int to_head = find_text_from_right(input , L , R , "TO");
 
     if(to_head != -1){
         int cas_tail = trim_and_call(parse_cas , input , L , to_head , output , OL);
         if(cas_tail != -1){
-            int kmh_head = match_string(input , to_head + 2 , R , "KMH");
+            int kmh_head = find_text_from_right(input , to_head + 2 , R , "KMH");
             if(kmh_head != -1){
                 int nnn_tail = trim_and_call(parse_nnn , input , to_head + 2 , kmh_head , output , cas_tail + 4);
                 memcpy(output + cas_tail , " TO " , sizeof(char) * 4);
@@ -83,7 +90,7 @@ int parse_change(char* input , int L , int R , char* output , int OL){
     return -1;
 }
 int parse_record(char* input , int L , int R , char* output , int OL ){
-    return R - L == 11 && match_string(input , L , R , "RECORD TIME") == L? OL + 11 : -1;
+    return parse_bold_text(input , L , R , output , OL , "RECORD TIME");
 }
 int parse_time_keeping(char* input , int L , int R , char* output , int OL){
     int record_tail = parse_record(input , L , R , output , OL);
@@ -135,10 +142,11 @@ int parse_sign(char* input , int L , int R , char* output , int OL ){
 }
 
 int parse_where(char* input , int L , int R , char* output , int OL){
-    if(match_string(input , L , R , "AT") == L){
+    int at_tail = parse_bold_text(input , L , L + 2 , output , OL , "AT ");
+    if(at_tail != -1){
         int sign_tail = trim_and_call(parse_sign , input , L + 2 , R  , output , OL + 3);
         if(sign_tail != -1){
-            memcpy(output + OL , "AT " , sizeof(char) * 3);
+            output[OL + 2] = ' ';
             return sign_tail;
         }
     }
@@ -146,50 +154,31 @@ int parse_where(char* input , int L , int R , char* output , int OL){
 }
 
 int parse_when(char* input , int L , int R , char* output , int OL ) {
-    if(match_string(input , L , R , "FIRST") == L && R - L == 5){
-        memcpy(output + OL , "FIRST" , sizeof(char) * 5);
-        return OL + 5;
-    }
-    if(match_string(input , L , R , "SECOND") == L && R - L == 6){
-        memcpy(output + OL , "SECOND" , sizeof(char) * 6);
-        return OL + 6;
-    }
-    if(match_string(input , L , R , "THIRD" ) == L && R - L == 5){
-        memcpy(output + OL , "THIRD" , sizeof(char) * 5);
-        return OL + 5;
-    }
-    return -1;
+    int first_tail = parse_bold_text(input , L , R , output , OL , "FIRST" );
+    if(first_tail != -1) return first_tail;
+    int second_tail = parse_bold_text(input , L , R , output , OL , "SECOND" );
+    if(second_tail != -1) return second_tail;
+    return parse_bold_text(input , L , R , output , OL , "THIRD" );
 }
 
 int parse_direction(char* input , int L , int R , char* output ,int OL ) {
-    if(match_string(input , L , R , "RIGHT") == L && R - L == 5){
-        memcpy(output + OL , "RIGHT" , sizeof(char) * 5);
-        return OL + 5;
-    }
-    if(match_string(input , L , R , "LEFT") == L && R - L == 4){
-        memcpy(output + OL , "LEFT" , sizeof(char) * 4);
-        return OL + 4;
-    }
-    return -1;
+    int right_tail = parse_bold_text(input , L , R , output , OL , "RIGHT");
+    if(right_tail != -1) return right_tail;
+    return parse_bold_text(input , L , R , output , OL , "LEFT");
 }
 
 int parse_how(char* input , int L , int R, char* output , int OL) {
-    if(R - L == 4 && match_string(input , L , R , "KEEP") == L){
-        memcpy(output + OL , "KEEP" , sizeof(char) * 4);
-        return OL + 4;
-    }
-    if(match_string(input , L , R , "GO") == L){
-        if(R - L == 2){
-            memcpy(output + OL , "GO" , sizeof(char) * 2);
-            return OL + 2;
-        }
-        int when_tail = trim_and_call(parse_when , input , L + 2 , R , output , OL + 3);
-        if(when_tail != -1) {
-            memcpy( output + OL , "GO ", sizeof(char) * 3); 
+    int go_tail = parse_bold_text(input , L , R , output , OL , "GO" );
+    if(go_tail != -1) return go_tail;
+    go_tail = parse_bold_text(input , L , L+2 , output , OL , "GO" );
+    if(go_tail != -1){
+        int when_tail = trim_and_call( parse_when , input , L+2 , R , output , OL + 3 );
+        if(when_tail != -1){
+            output[OL + 2] = ' ';
             return when_tail;
         }
     }
-    return -1;
+    return parse_bold_text(input , L , R , output , OL , "KEEP");
 }
 
 int parse_directional(char* input , int L , int R , char* output , int OL ) {
@@ -197,20 +186,20 @@ int parse_directional(char* input , int L , int R , char* output , int OL ) {
     for(i = L + 1 ; i < R ; i++ ){
         int how_tail = trim_and_call(parse_how , input , L , i , output , OL );
         if(how_tail == -1) continue;
-
+        //printf("how tail: %d\n" , how_tail);
         int direction_tail = trim_and_call(parse_direction , input , i ,  R , output , how_tail + 1);
         if(direction_tail != -1) {
             output[how_tail] = ' ';
             return direction_tail;
         }
-
         for(j = i ; j < R ; j ++){
 
             direction_tail = trim_and_call(parse_direction , input , i ,  j , output , how_tail + 1);
             if(direction_tail == -1) continue;
-
+            //printf("direction_tail %d L: %d R: %d\n" , direction_tail , j , R);
             int where_tail = trim_and_call(parse_where, input , j , R , output , direction_tail + 1);
             if(where_tail != -1){
+                //printf("where_tail: %d\n" , where_tail);
                 output[how_tail] = output[direction_tail] = ' ';
                 return where_tail;
             }
@@ -220,9 +209,9 @@ int parse_directional(char* input , int L , int R , char* output , int OL ) {
 }
 
 int parse_navigational(char* input , int L , int R , char* output , int OL) {
-    int and_then_head = match_string(input , L , R , "AND THEN");
+    int and_then_head = find_text_from_right(input , L , R , "AND THEN");
     
-    if( and_then_head != -1){
+    if(and_then_head != -1){
         int navigational_tail = trim_and_call(parse_navigational , input , L , and_then_head , output , OL );
         
         if(navigational_tail != -1){
@@ -234,31 +223,33 @@ int parse_navigational(char* input , int L , int R , char* output , int OL) {
                 return directional_tail;
             }
         }
-        
     }
 
-    return parse_directional(input , L , R , output , OL);
+    int directional_tail = parse_directional(input , L , R , output , OL);
+    //printf("directional_tail: %d L:%d R:%d\n" , directional_tail, L , R);
+    if(directional_tail != -1)
+        return directional_tail;
+    return -1;
 }
 
 
 int parse_instruction(char* input , int L , int R , char* output , int OL) {
-    int and_head = match_string(input , L , R , "AND");
+    int and_head = find_text_from_right(input , L , R , "AND");
+
     if( and_head != -1){
-        printf("and_head: %d\n" , and_head);
         int navigational_tail = trim_and_call(parse_navigational, input , L , and_head , output , OL );
         if(navigational_tail != -1){
-            printf("nav_tail: %d\n" , navigational_tail);
             int time_keeping_tail = trim_and_call(parse_time_keeping ,input , and_head + 3, R , output , navigational_tail + 5);
             if(time_keeping_tail != -1){
-                printf("time_keeping_tail: %d\n" , time_keeping_tail);
                 memcpy( output + navigational_tail , " AND " , sizeof(char) * 5);
                 return time_keeping_tail;
             }
         }
-
+        //printf("hello %d\n" , navigational_tail);
     }
 
     int navigational_tail = parse_navigational( input , L , R , output , OL );
+    //printf("nav_tail: %d\n" , navigational_tail);
     if(navigational_tail != -1)
         return navigational_tail;
     return  parse_time_keeping(input , L , R , output , OL );
